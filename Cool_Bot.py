91119d599ae4a05225260795e22abcb0ab56b9cb
TevoBaudejas <75721196+TevoBaudejas@users.noreply.github.com>
2021-01-24 13:01:40 -0300

    Update Cool_Bot.py

--- Cool_Bot.py
+++ Cool_Bot.py
@@ -20,15 +20,12 @@ async def on_ready():
     print('Connected to bot: {}'.format(client.user.name))
     print('Bot ID: {}'.format(client.user.id))
 
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="Lorem Ipsum asdasd", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
-    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
-    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
-    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
-    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-    # embed.set_thumbnail(url=f"{ctx.guild.icon}")
-    embed.set_thumbnail(url="https://pluralsight.imgix.net/paths/python-7be70baaac.png")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("%hello"):
+        await message.channel.send("Hey there buddy!")
 
 client.run(os.getenv('TOKEN'))
 
