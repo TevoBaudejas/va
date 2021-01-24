562d45bbd0fba3152ee8e36df54752c82ee0aa82
TevoBaudejas <75721196+TevoBaudejas@users.noreply.github.com>
2021-01-24 13:21:24 -0300

    Update Cool_Bot.py

--- Cool_Bot.py
+++ Cool_Bot.py
@@ -10,10 +10,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("We are ready to rol out".format(client))
-    
-# Setting `Watching ` status
-await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="🧙🏻MAGIJOS MINISTERIJA VIRŠ MANĘS🧙"))
 
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game('🧙🏻MAGIJOS MINISTERIJA VIRŠ MANĘS🧙'))
+    
     print('Connected to bot: {}'.format(client.user.name))
     print('Bot ID: {}'.format(client.user.id))
 
@@ -25,6 +27,5 @@ async def on_message(message):
         await message.channel.send("Hey there buddy!")
         
 client.run(os.getenv('TOKEN'))
-
 
 
