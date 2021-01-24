ac5ac2357b4566a8a1f46a0012347375d54ed040
TevoBaudejas <75721196+TevoBaudejas@users.noreply.github.com>
2021-01-24 13:12:34 -0300

    Update Cool_Bot.py

--- Cool_Bot.py
+++ Cool_Bot.py
@@ -10,14 +10,11 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("We are ready to rol out".format(client))
-   
-    @client.event
+
+
+@client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name='MAGIJOS MINISTERIJA VIRS MANES', url='https://www.twitch.tv/your_channel_here'))
-    
-    print('Connected to bot: {}'.format(client.user.name))
-    print('Bot ID: {}'.format(client.user.id))
-    
+    await client.change_presence(activity=discord.Game('🧙🏻MAGIJOS MINISTERIJA VIRŠ MANĘS🧙🏻'))
     
     print('Connected to bot: {}'.format(client.user.name))
     print('Bot ID: {}'.format(client.user.id))
@@ -28,5 +25,8 @@ async def on_message(message):
         return
     if message.content.startswith("%hello"):
         await message.channel.send("Hey there buddy!")
-
+        
 client.run(os.getenv('TOKEN'))
+
+
+
